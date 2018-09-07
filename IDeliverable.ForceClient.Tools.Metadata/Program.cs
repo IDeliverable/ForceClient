ba2f0aa9183edfa139ac4bb046fd2459e1810565
Daniel Rosenberg <daniel@ideliverable.com>
2018-09-07 21:40:17 +0200

    Fixed OIDC integration.

--- IDeliverable.ForceClient.Tools.Metadata/Program.cs
+++ IDeliverable.ForceClient.Tools.Metadata/Program.cs
@@ -12,16 +12,17 @@ namespace IDeliverable.ForceClient.Tools.Metadata
             {
                 Authority = "https://login.salesforce.com/",
                 ClientId = "3MVG9A_f29uWoVQvrJSnfk5LPeA2zP4q_U4piOC.9D9E0xzbHOmSZJYroajSEEGlK32K_X9i66uunCW3BBCnE",
+                ClientSecret = "2763444084747273086",
                 RedirectUri = "http://localhost:7890/",
-                //Scope = "api refresh_token offline_access",
                 Flow = OidcClientOptions.AuthenticationFlow.AuthorizationCode,
                 ResponseMode = OidcClientOptions.AuthorizeResponseMode.Redirect,
                 Browser = new SystemBrowser(port: 7890)
             };
 
             var client = new OidcClient(options);
+            var request = new LoginRequest();
 
-            var result = await client.LoginAsync();
+            var result = await client.LoginAsync(request);
             var token = result.AccessToken;
 
             Console.WriteLine(token);
